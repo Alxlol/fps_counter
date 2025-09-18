172dcfd8065b1e922046aca9eaa2a719f500830c
Alexander Friberg <friberg.alx@gmail.com>
2025-09-18 20:42:18 +0200

    updated example project

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,61 +4,38 @@ import 'package:fps_counter/fps_counter.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   FpsCounter.initialize();
-  runApp(const MyApp());
+  runApp(const ExampleApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ExampleApp extends StatelessWidget {
+  const ExampleApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'FPS Counter example app',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 6,
+            children: <Widget>[
+              OutlinedButton(
+                  onPressed: FpsCounter.hideFps, child: Text('Hide')),
+              OutlinedButton(
+                  onPressed: FpsCounter.showFps, child: Text('Show')),
+              OutlinedButton(
+                  onPressed: () => FpsCounter.setSmoothing(false),
+                  child: Text('Disabled smoothing')),
+              OutlinedButton(
+                  onPressed: () => FpsCounter.setSmoothing(true),
+                  child: Text('Enable smoothing'))
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
